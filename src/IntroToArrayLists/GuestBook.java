afc309d6b6d94fa31e397f6468781f1b7e6d9848
Oliver Nizet <league@WTS14-4.attlocal.net>
2018-05-15 18:20:18 -0700

    frickel pickel

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,12 +1,67 @@
 package IntroToArrayLists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	JFrame frame;
+	JPanel panel;
+	JButton b1;
+	JButton b2;
+	ArrayList<String> names = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		GuestBook gb = new GuestBook();
+	}
+
+	GuestBook() {
+		frame = new JFrame();
+		panel = new JPanel();
+		b1 = new JButton();
+		b2 = new JButton();
+		b1.setText("Add Name");
+		b2.setText("View Names");
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		panel.add(b1);
+		panel.add(b2);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == b1) {
+			String name = JOptionPane.showInputDialog("Enter a Name");
+			names.add(name);
+		}
+		if (e.getSource() == b2) {
+			String gl = "";
+			for (int i = 0; i < names.size(); i++) {
+				gl += "Guest #" + (i + 1) + ": " + names.get(i) + "\n";
+
+			}
+			JOptionPane.showMessageDialog(null, gl);
+		}
+	}
 }
--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,29 +1,46 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
-		
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
-		
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
+		// 1. Create a Stack of Doubles
+		// Don't forget to import the Stack class
+		Stack<Double> doubles = new Stack<Double>();
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		for (int i = 0; i < 100; i++) {
+			double ran = new Random().nextDouble() * 100;
+			doubles.push(ran);
+		}
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+		String n1 = JOptionPane.showInputDialog("Enter A Number (1-100)");
+		String n2 = JOptionPane.showInputDialog("Enter Higher Number (1-100)");
+		int i1 = Integer.parseInt(n1);
+		int i2 = Integer.parseInt(n2);
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that
+		// is
+		// between the two numbers entered by the user, print it to the screen.
+		for (int i = 0; i < doubles.size(); i++) {
+			Double d = doubles.pop();
+			if (d > i1 && d < i2) {
+				System.out.println(d);
+			}
+		}
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
+
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
 	}
 }
